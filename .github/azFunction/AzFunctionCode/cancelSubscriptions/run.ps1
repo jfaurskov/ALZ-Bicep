2e5e66858277ecfc60e89ecad7368e7d633fd923
Jan Faurskov <22591930+jfaurskov@users.noreply.github.com>
2022-07-12 09:55:02 +0200

    Add initial cancel

--- .github/azFunction/AzFunctionCode/cancelSubscriptions/run.ps1
+++ .github/azFunction/AzFunctionCode/cancelSubscriptions/run.ps1
@@ -5,7 +5,9 @@ Write-Host "PowerShell queue trigger function processed work item: $QueueItem"
 $subscriptionId = $QueueItem.Body.subscriptionId
 $subscriptionName = $QueueItem.Body.subscriptionName
 Write-Host "Subscription to be canceled is $subscriptionName with id: $subscriptionId"
-
+$cancelUri = "https://management.azure.com/subscriptions/$($subscriptionId)/providers/Microsoft.Subscription/cancel?api-version=2020-09-01"
+$token = (Get-AzAccessToken).Token | ConvertTo-SecureString -AsPlainText -Force
+Write-Host "Invoke-RestMethod -Method Post -ContentType "application/json" -Authentication Bearer -Token $token -Uri $cancelUri"
 #MSI to look for subscripition in current tenant
 # fixme some code to cancel subscription and possibly verify that it happened
 $body = @{
